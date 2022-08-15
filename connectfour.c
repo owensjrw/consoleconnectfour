a1faffb083723a343ed3511fb2d3fc6e2467fe93
William Owens <owensjrw@gmail.com>
2022-08-15 08:03:14 -0500

    Update connectfour.c

--- connectfour.c
+++ connectfour.c
@@ -1,7 +1,33 @@
 #include "connectfour.h"
 #include <stdio.h>
 
-board_t board = {0};
+board_t board;
+
+void newBoard(void) {
+  for (int i = 0; i < BOARDROW; i++) {
+    for (int j = 0; j < BOARDCOLUMN; j++) {
+      board[i][j] = 'O';
+    }
+  }
+}
+
+void paintBoard(void) {
+  puts("");
+  for (int i = 0; i < BOARDROW + 1; i++) {
+    for (int j = 0; j < BOARDCOLUMN; j++) {
+      if (i == 0) {
+        printf("  ");
+        printf("%c ", (ASCIIZERO + j + 1));
+      } else {
+        printf("| ");
+        printf("%c ", board[i - 1][j]);
+        if (j == BOARDCOLUMN - 1)
+          printf("|");
+      }
+    }
+    printf("\n=============================\n");
+  }
+}
 
 int getPlayers(void) {
   char players;
@@ -13,4 +39,16 @@ int getPlayers(void) {
 
 int validPlayerNumber(int players) {
   return (players == 1 || players == 2);
+}
+
+int placeToken(void) {
+  char playColumn;
+  puts("What column would you like to place your token in?");
+  scanf("%c", &playColumn);
+  while (getchar() != '\n');
+  return playColumn - ASCIIZERO;
+}
+
+int validPlaceToken(int tokenColumn) {
+  return (tokenColumn >= 1 && tokenColumn <= 7);
 }
