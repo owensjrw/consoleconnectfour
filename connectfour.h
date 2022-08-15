52e946bb42f87ae8afc969b7d945076a1ed60644
William Owens <owensjrw@gmail.com>
2022-08-15 08:02:29 -0500

    Update connectfour.h

--- connectfour.h
+++ connectfour.h
@@ -3,13 +3,18 @@
 
 #define ASCIIZERO 48
 #define INVALIDPLAYERS -1
-#define BOARDROW 7
-#define BOARDCOLUMN 6
+#define BOARDROW 6
+#define BOARDCOLUMN 7
 
-typedef char board_t[BOARDCOLUMN][BOARDROW];
+typedef char board_t[BOARDROW][BOARDCOLUMN];
 extern board_t board;
 
 int getPlayers(void);
 int validPlayerNumber(int);
+void newBoard(void);
+void paintBoard(void);
+int placeToken(void);
+int validPlaceToken(int);
+
 
 #endif
