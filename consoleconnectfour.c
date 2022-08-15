a3d8807f3a9ef328d883235fbc489703d2f0ca31
William Owens <owensjrw@gmail.com>
2022-08-15 08:04:15 -0500

    Update consoleconnectfour.c

--- consoleconnectfour.c
+++ consoleconnectfour.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
 #include "connectfour.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int main(void) {
   int num_players = INVALIDPLAYERS;
   while (!validPlayerNumber(num_players))
     num_players = getPlayers();
-  for (int i = 0; i < BOARDCOLUMN; i++) {
-    for (int j = 0; i < BOARDROW; j++) {
-      printf("%c\t", board[i][j]);
-    }
-    printf("--\t");
-  }
+  newBoard();
+  system("clear");
+  paintBoard();
   return 0;
 
 }
